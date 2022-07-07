c7578b599093deddefe06fea7d10d0290175ad22
yohanafitria <yohana_009@smkassalaambandung.sch.id>
2022-07-07 14:14:54 +0700

    Review

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+class Database
+{
+    public $host = "localhost", $user = "root", $pass = "", $db = "bd1";
+    public $koneksi;
+
+    public function __construct()
+    {
+        $this->koneksi = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->db
+        );
+        if ($this->koneksi) {
+            //echo "koneksi berhasil";
+        } else {
+            echo "Koneksi Database Gagal";
+        }
+    }
+}
+// Data Tabel Guru
+include 'guru.php';
+
+// data tabel A
+// include ''
+
+// koneksi DB
+$db = new Database();
